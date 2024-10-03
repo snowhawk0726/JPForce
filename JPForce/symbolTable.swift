7cfcbd384f72ce6bdea34fb1afa1dc15ca905c82
Takayuki Sato <SnowHawk0726@gmail.com>
2024-10-03 14:07:11 +0900

    Add Global Variable (and Symbol Table).Modified stack specification (to match the interpreter) 1. グローバル変数サポート     1. opSetGlobal, opGetGlobal     2. DefineStatement, Identifierのcompiled(with:)を追加     3. GlobalStore追加         1. globals配列初期化         2. アクセサ     4. シンボルテーブル追加         1. シンボル名とシンボルの辞書         2. シンボルは、名前とスコープと定義位置を持つ         3. define(_:)で定義、resolve(_:)で取得     5. testGlobalDefStatements()追加     6. SymbolTableTestsを追加 2. スタック仕様を変更(インタープリタに合わせた)     1. Programコンパイル時のopPopを廃止     2. CaseExpressionコンパイル変更     3. CompilerTestsを修正 3. Compiler、VMのconstantsを外部入力可能に変更     1. スタックトップ→最終pop 4. VMのstackをクラス化(Stack)     1. stack配列初期化     2. 各種アクセス関数をサポート 5. Replで、constants,  symbolTable, globals, stackを引き継ぎ可能に修正

---
+++ JPForce/symbolTable.swift
@@ -0,0 +1,8 @@
+//
+//  symbolTable.swift
+//  JPForce
+//
+//  Created by 佐藤貴之 on 2024/09/30.
+//
+
+import Foundation
---
+++ UnitTests/SymbolTableTests.swift
@@ -0,0 +1,16 @@
+//
+//  SymbolTableTests.swift
+//  JPForce
+//
+//  Created by 佐藤貴之 on 2024/09/30.
+//
+
+import Testing
+
+struct SymbolTableTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
